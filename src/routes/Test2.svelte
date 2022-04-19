61971ad61ca7b51b37999a1a7c965f5f3ab2a5cf
caboe <ca_boe@yahoo.de>
2022-04-19 20:19:17 +0200

    Add example for gQuery fetch

---
+++ src/routes/Test2.svelte
@@ -0,0 +1,31 @@
+<script context="module">
+	import { getBooks, Books } from '$lib/Books.gq';
+
+	export async function load({ fetch }) {
+		await getBooks({ fetch });
+		return {};
+	}
+</script>
+
+<h1>Books</h1>
+
+{#each $Books.books as { title, author }}
+	<div>
+		<h4>{title}</h4>
+		<cite>-- {author}</cite>
+	</div>
+{/each}
+
+<style>
+	div {
+		display: grid;
+		border: 1px solid #ddd;
+		max-width: 400px;
+		padding: 1rem;
+		margin-bottom: 1rem;
+		background-color: lightgoldenrodyellow;
+	}
+	h4 {
+		margin: 0;
+	}
+</style>
